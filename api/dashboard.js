4c941f39efeb3d46e39b122e07bf0452ccbc42fe
SABI9666 <sabincn676@gmail.com>
2025-10-15 20:19:18 +0530

    Update dashboard.js

--- api/dashboard.js
+++ api/dashboard.js
@@ -6,7 +6,7 @@ const util = require('util');
 const db = admin.firestore();
 
 const allowCors = fn => async (req, res) => {
-    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Credentials', false); 
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization');
